a80f6ae15ab38c0c4b582839383c0387964b5197
tasi <daunte_west1@baylor.edu>
2023-02-22 10:17:46 -0600

    working on exercise

--- PersonClass.py
+++ PersonClass.py
@@ -21,4 +21,4 @@ class Customer(Person):
         self.on_mail_list = on_mail_list
 
     def print_person(self):
-        print(f"Name: {self.__name} Address: {self.__addr} Phone: {self.__phone} Customer Number: {self.__customer_number}, On Mailing List: {self.__on_mailing_list}")
+        print(f"Name: {self.__name} Address: {self.__addr} Phone: {self.__phone} Customer Number: {self.__cust_num}, On Mailing List: {self.__on_mailing_list}")
--- PersonProgram.py
+++ PersonProgram.py
@@ -3,19 +3,19 @@ import PersonClass as pc
 
 def main():
     person = pc.Person('John', '1234 North Central Street', '289-337-4281')
-    # customer = pc.Customer('Nathan', '2345 Southlake Avenue',
-    # '314-986-1114', '892582443', True)
+    customer = pc.Customer('Nathan', '2345 Southlake Avenue',
+                           '314-986-1114', '892582443', True)
 
     show_person_info(person)
-    # show_person_info(customer)
+    show_person_info(customer)
 
 
-def show_person_info(person):
-    if isinstance(person, pc.Person):
-        person.print_person()
+def show_person_info(human):
+    if isinstance(human, pc.Person):
+        human.print_person()
     else:
 
-        print(f"{person} is not from the person class")
+        print(f"{human} is not from the person class")
 
 
 main()
