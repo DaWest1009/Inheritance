d4085dbc3166abb612ed4ce84712278c87b3daef
tasi <daunte_west1@baylor.edu>
2023-04-17 09:42:49 -0500

    study for quiz ii

--- PersonClass.py
+++ PersonClass.py
@@ -33,9 +33,22 @@ class Customer(Person):
     def print_person(self):
         # to access superclass attributes just call the method
         Person.print_person(self)
-
+        '''
+        #or can do it this way 
+        print('Name:', self._Person__name)
+        print('Address:', self._Person__addr)
+        print('Phone:', self._Person__phone)
+        '''
         print("Customer Number:",  self.__cust_num)
         if self.__on_mail_list:  # aka if self.__on_mail_list == True
             print("On Mailing List: Yes")
         else:
             print("Off Mailing List: No")
+
+'''
+In the above example, the Superclass has hidden attributes named __name, __addr, and __phone. 
+When the Subclass inherits from Superclass, it also inherits these hidden attributes. 
+However, since the attribute is name-mangled by the interpreter, it cannot be accessed directly by the Subclass. 
+Instead, we have to use the name-mangled version of the attribute name (_Person__name) 
+to access the attribute in the Subclass.
+'''
