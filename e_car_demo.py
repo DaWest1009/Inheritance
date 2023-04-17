2ccb0c6a6117a997859a7ac19a548df7324e0e2e
tasi <daunte_west1@baylor.edu>
2023-04-17 11:59:51 -0500

    x

--- e_car_demo.py
+++ e_car_demo.py
@@ -1,6 +1,6 @@
 # This program demonstrates the Car class.
 
-import vehicles
+import e_vehicles as vehicles
 
 def main():
     # Create an object from the Car class.
@@ -9,6 +9,14 @@ def main():
     used_car = vehicles.Car('Audi', 2007, 12500, 21500.00, 4)
 
     # Display the car's data.
+    print('Make:', used_car.get_make())
+    print('Model:', used_car.get_model())
+    print('Mileage:', used_car.get_mileage())
+    print('Price:', used_car.get_price())
+    print('Number of doors:', used_car.get_doors())
+
+    used_car.set_make('BMW')
+
     print('Make:', used_car.get_make())
     print('Model:', used_car.get_model())
     print('Mileage:', used_car.get_mileage())
